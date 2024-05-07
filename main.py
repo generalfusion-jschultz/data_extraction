cbd27765f002b05f81419a0b1f1348bdedf89cd3
Jason Schultz <jason.schultz@generalfusion.com>
2024-05-06 17:03:43 -0700

    Added test conditions and created dataclass as client arguement

--- main.py
+++ main.py
@@ -8,23 +8,12 @@
 """Subscribes to published MQTT nodes and periodically writes them to a csv file."""
 # ---------------------------------------------------------------------------
 from data_extraction.client import DataExtractionClient
-import pandas as pd
-
-# TODO: - Write test conditions
-#       - Look into limit condition for interpolating sparse data
-#       - May need to lock variables in threads when accessing them
-#       - Add checks for output folder directories
-#       - Resampling the interpolated data at rates < 1s gives sparse results
-#           - Might be due to being lower frequency than publish rate?
-#           - Might need to resample first then interpolate?
 
 def main():
     client = DataExtractionClient()
     client.connect()
     client.subscribe(topic = "prototype-zero/#")
-    client.run()
-
-    # client.end_of_day(2024, 5, 1)    
+    client.run() 
 
 
 if __name__ == "__main__":
--- src/data_extraction/client.py
+++ src/data_extraction/client.py
@@ -20,60 +20,87 @@ from threading import (Thread, Lock)
 import json
 import logging
 from collections import deque
+from dataclasses import dataclass
 
 
 config = initialize(
     config="./config/config.toml", secrets=".env", logger="./config/logger.yaml"
 )
-BROKER_CONFIG = config["mqtt"]["broker"]
-TOPIC_STRUCTURE = config["mqtt"]["node_network"]["topic_structure"]
-
-CLIENT_NAME = config["data_extraction"]["name"]
-CLIENT_NODE_ID = config["data_extraction"]["node_id"]
-ID_STRUCTURE = config["data_extraction"]["id_structure"]
-MAX_BUFFER_LENGTH = config["data_extraction"]["max_buffer_length"]
-MAX_BUFFER_TIME = config["data_extraction"]["max_buffer_time"]
-RESAMPLE_TIME = config["data_extraction"]["resample_time"]
-RAW_OUTPUT_FILENAME = config["data_extraction"]["raw_ouput_filename"]
-PROCESSED_FILENAME = config["data_extraction"]["processed_output_filename"]
-SUBSCRIPTIONS = config["data_extraction"]["subscriptions"]
-
 logger = logging.getLogger("__name__")
+BROKER_CONFIG = config["mqtt"]["broker"]
+
+
+@dataclass
+class DataExtractionConfig():
+    name: str
+    node_id: str
+    max_buffer_length: int
+    max_buffer_time: float
+    subscriptions: str | list[str]
+    topic_structure: str
+    id_structure: str
+    resample_time: float
+    nan_limit: int
+    output_filename: str
+    output_directory: str
+    processed_output_filename: str
+    processed_output_directory: str
+
+    def __post_init__(self):
+        if not isinstance(self.max_buffer_length, int):
+            raise TypeError("max_buffer_length needs to be an integer")
+        if not isinstance(self.nan_limit, int):
+            raise TypeError("nan_limit needs to be an integer")
+
+
+EXTRACTION_CONFIG = DataExtractionConfig(
+    name = config["data_extraction"]["name"],
+    node_id = config["data_extraction"]["node_id"],
+    max_buffer_length = config["data_extraction"]["max_buffer_length"],
+    max_buffer_time = config["data_extraction"]["max_buffer_time"],
+    subscriptions = config["data_extraction"]["subscriptions"],
+    topic_structure = config["mqtt"]["node_network"]["topic_structure"],
+    id_structure = config["data_extraction"]["id_structure"],
+    resample_time = config["data_extraction"]["resample_time"],
+    nan_limit = config["data_extraction"]["nan_limit"],
+    output_filename = config["data_extraction"]["ouput_filename"],
+    output_directory = config["data_extraction"]["output_directory"],
+    processed_output_filename = config["data_extraction"]["processed_output_filename"],
+    processed_output_directory = config["data_extraction"]["processed_output_directory"]
+)
+
 
 class DataExtractionClient(MQTTClient):
     def __init__(
             self,
             broker_config: MQTTBrokerConfig = BROKER_CONFIG,
-            name: str = CLIENT_NAME,
-            node_id: str = CLIENT_NODE_ID,
-            node_type = None,
-            max_buffer: int = MAX_BUFFER_LENGTH,
-            resample_time_seconds: float = RESAMPLE_TIME,
-            buffer_time_interval: int = MAX_BUFFER_TIME,
-            subscriptions: str | list[str] = SUBSCRIPTIONS,  # Currently unused
-            topic_structure: str = TOPIC_STRUCTURE,
-            id_structure: str = ID_STRUCTURE,
-            raw_output_filename: str = RAW_OUTPUT_FILENAME,
-            processed_output_filename: str = PROCESSED_FILENAME
+            data_extraction_config = EXTRACTION_CONFIG,
+            logger = None
         ):
         MQTTClient.__init__(
             self,
             broker_config = broker_config,
-            name = name,
-            node_id = node_id,
-            node_type = node_type,
-            logger = None,
+            name = data_extraction_config.name,
+            node_id = data_extraction_config.node_id,
+            node_type = None,
+            logger = logger,
             buffer = deque(),
-            topic_structure = topic_structure,
+            topic_structure = data_extraction_config.topic_structure,
         )
         self.start_time = datetime.now()
         self.lock = Lock()
-        self.max_buffer = max_buffer
-        self.resample_time_seconds = resample_time_seconds
-        self.buffer_time_interval = buffer_time_interval
-        self.raw_output_filename = raw_output_filename
-        self.processed_output_filename = processed_output_filename
-        self.id_structure = id_structure
+        self.max_buffer = data_extraction_config.max_buffer_length
+        self.resample_time_seconds = data_extraction_config.resample_time
+        self.buffer_time_interval = data_extraction_config.max_buffer_time
+        self.output_filename = data_extraction_config.output_filename
+        self.processed_filename = data_extraction_config.processed_output_filename
+        self.id_structure = data_extraction_config.id_structure
+        self.output_directory = data_extraction_config.output_directory
+        self.processed_directory = data_extraction_config.processed_output_directory
+        self.nan_limit = data_extraction_config.nan_limit
+
+        os.makedirs(self.output_directory, exist_ok=True)
+        os.makedirs(self.processed_directory, exist_ok=True)
 
 
     def check_message_value(self, message: MQTTMessage):
@@ -196,23 +223,45 @@ class DataExtractionClient(MQTTClient):
             time.sleep(60)
 
 
+    def obtain_df(self, year, month, day) -> pd.DataFrame | None:
+        try:
+            df = pd.read_csv(f"{self.raw_directory}/{self.raw_output_filename}_{year}_{month}_{day}.csv")
+        except FileNotFoundError:
+            logger.info(f"{self.raw_output_filename} file not found. No data to process.")
+            return None
+        else:
+            return df
+
+
     def end_of_day(self, year, month, day) -> None:
-        df = pd.read_csv(f"output/{self.raw_output_filename}_{year}_{month}_{day}.csv")
+        df = self.obtain_df(year, month, day)
+        if df is None:
+            return
         df = self.process_data_pandas(df)
-        filename = f"processed_output/{self.processed_output_filename}"
+        os.makedirs(self.processed_directory, exist_ok=True)
+        filename = f"{self.processed_directory}/{self.processed_filename}_{year}_{month}_{day}.csv"
         self.start_time = datetime.now()
         self.write_to_file(df, filename)
+
+
+    def interp_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        df["time"] = pd.to_datetime(df["time"])
+        df = df.drop(columns = "topic")
+        df = df.pivot(index = "time", columns = ["id"], values = "value")
+        df = df.interpolate(method = "time", limit = self.nan_limit)
+        return df
+    
+    
+    def resample_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.resample(rule = f"{self.resample_time_seconds}s").mean()
 
 
     # Look into this for interpolating with conditions:
     #   https://stackoverflow.com/questions/69951782/pandas-interpolate-with-condition
     def process_data_pandas(self, df: pd.DataFrame) -> pd.DataFrame:
         start = time.perf_counter()
-        df["time"] = pd.to_datetime(df["time"])
-        df = df.drop(columns = "topic")
-        df = df.pivot(index = "time", columns = ["id"], values = "value")
-        df = df.interpolate()
-        df = df.resample(rule = f"{self.resample_time_seconds}s").mean()
+        df = self.interp_df(df)
+        df = self.resample_df(df)
         stop = time.perf_counter()
         performance_time = stop - start
         logger.info(f"Pandas performance time: {performance_time}")
@@ -228,7 +277,9 @@ class DataExtractionClient(MQTTClient):
             buffer_time_exceeded = (datetime.now() - compare_time).total_seconds() > self.buffer_time_interval
             if buffer_length_exceeded or (buffer_time_exceeded and self.buffer):
                 update_list = [self.buffer.popleft() for i in range(current_buffer_length)]
-                self.update_csv(update_list, filename = f"output/{self.raw_output_filename}")
+                os.makedirs(self.output_directory, exist_ok=True)
+                filename = f"{self.output_directory}/{self.output_filename}_{self.start_time.year}_{self.start_time.month}_{self.start_time.day}.csv"
+                self.update_csv(update_list, filename)
                 compare_time = datetime.now()
 
             time.sleep(1)
@@ -246,10 +297,7 @@ class DataExtractionClient(MQTTClient):
             filename: str,
             append: bool = False
         ) -> None:
-        year = self.start_time.year
-        month = self.start_time.month
-        day = self.start_time.day
-        output_path = filename + f"_{year}_{month}_{day}.csv"
+        output_path = filename
 
         if append:
             df.to_csv(output_path, mode = 'a', header = not os.path.exists(output_path), index = False)
